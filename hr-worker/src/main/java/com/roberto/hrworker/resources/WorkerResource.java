0f646f56992a11de5625df668f7eedd953f913fc
jrpbjr <jrpbjr@gmail.com>
2020-12-20 18:34:02 -0300

    mapeando end point e persistencia

--- hr-worker/src/main/java/com/roberto/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/roberto/hrworker/resources/WorkerResource.java
@@ -1,9 +1,14 @@
 package com.roberto.hrworker.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.roberto.hrworker.entities.Worker;
 import com.roberto.hrworker.repositories.WorkerRepository;
 
 @RestController
@@ -12,5 +17,11 @@ public class WorkerResource {
 	
 	@Autowired
 	private WorkerRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll(){
+		List<Worker> list = repository.findAll();
+		return ResponseEntity.ok(list);
+	}
 
 }
