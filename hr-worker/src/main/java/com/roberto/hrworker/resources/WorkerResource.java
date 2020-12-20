051988fc1d90f1003faffec9f4bb8baf94d6485b
jrpbjr <jrpbjr@gmail.com>
2020-12-20 18:19:03 -0300

    injetando dependencia pelo framework

--- hr-worker/src/main/java/com/roberto/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/roberto/hrworker/resources/WorkerResource.java
@@ -1,5 +1,16 @@
 package com.roberto.hrworker.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.roberto.hrworker.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	@Autowired
+	private WorkerRepository repository;
 
 }
