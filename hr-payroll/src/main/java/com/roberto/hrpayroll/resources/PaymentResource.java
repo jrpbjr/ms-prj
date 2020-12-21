3725c090e5cf9bf3c5b2cd155f4743472e4d9118
jrpbjr <jrpbjr@gmail.com>
2020-12-20 22:34:18 -0300

    iniciando rota payment

--- hr-payroll/src/main/java/com/roberto/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/resources/PaymentResource.java
@@ -1,9 +1,13 @@
 package com.roberto.hrpayroll.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.roberto.hrpayroll.entities.Payment;
 import com.roberto.hrpayroll.services.PaymentService;
 
 @RestController
@@ -12,5 +16,10 @@ public class PaymentResource {
 	
 	@Autowired
 	private PaymentService service;
+	
+	@GetMapping(value = "/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+		
+	}
 
 }
