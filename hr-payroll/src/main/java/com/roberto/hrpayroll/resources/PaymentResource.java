7bd14f90ecfb841a01ab258b8ef9b78885e72a59
jrpbjr <jrpbjr@gmail.com>
2020-12-20 22:27:36 -0300

    injetando o serviço de pagamento

--- hr-payroll/src/main/java/com/roberto/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/resources/PaymentResource.java
@@ -1,12 +1,16 @@
 package com.roberto.hrpayroll.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.roberto.hrpayroll.services.PaymentService;
 
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
 	
-	
+	@Autowired
+	private PaymentService service;
 
 }
