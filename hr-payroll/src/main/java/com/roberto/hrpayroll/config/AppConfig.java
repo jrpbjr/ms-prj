86ce0129a9f3c9c501e2a1324ddd39fed15bd39a
jrpbjr <jrpbjr@gmail.com>
2020-12-21 21:08:29 -0300

    instancia unica singleton

--- hr-payroll/src/main/java/com/roberto/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/config/AppConfig.java
@@ -1,8 +1,12 @@
 package com.roberto.hrpayroll.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
+ 
+	public RestTemplate restTemplate() {
+		
+	}
 }
