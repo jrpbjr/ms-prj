7aefd43487e90050b98ddddc435f0f48b24e19ca
jrpbjr <jrpbjr@gmail.com>
2020-12-22 19:17:22 -0300

    clinte do projeto hr-worker

--- hr-payroll/src/main/java/com/roberto/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/HrPayrollApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
