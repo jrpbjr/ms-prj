51efbc0234a598d6d7d104542bd05f291a5610c4
jrpbjr <jrpbjr@gmail.com>
2020-12-21 21:11:56 -0300

    criar componenete a partir de uma chamada de um metodo

--- hr-payroll/src/main/java/com/roberto/hrpayroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/config/AppConfig.java
@@ -1,11 +1,13 @@
 package com.roberto.hrpayroll.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
  
+	@Bean
 	public RestTemplate restTemplate() {
 		
 	}
