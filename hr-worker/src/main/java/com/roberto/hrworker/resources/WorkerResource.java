23a4ea6ceada19ecd53454b7fa8052abe9fa263f
jrpbjr <jrpbjr@gmail.com>
2020-12-22 19:33:14 -0300

    objeto Enviroment contem varios contexto da aplicação, condigurações apenas para testes contem hard code

--- hr-worker/src/main/java/com/roberto/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/roberto/hrworker/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package com.roberto.hrworker.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,9 @@ import com.roberto.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private WorkerRepository repository;
