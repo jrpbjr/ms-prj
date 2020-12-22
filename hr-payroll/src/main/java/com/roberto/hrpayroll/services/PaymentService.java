add2eca1a7d604238692f0a5bb4334a4a835c9d1
jrpbjr <jrpbjr@gmail.com>
2020-12-21 21:19:28 -0300

    injeção da dependencia restTemplate

--- hr-payroll/src/main/java/com/roberto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/services/PaymentService.java
@@ -1,11 +1,16 @@
 package com.roberto.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.roberto.hrpayroll.entities.Payment;
 
 @Service
 public class PaymentService {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	public Payment getPayment(long workerId, int days) {
 		return new Payment("Bob",200.0,days);
