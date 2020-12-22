78f4f81e88915e2dcab39d453dc658ccbf7f3e37
jrpbjr <jrpbjr@gmail.com>
2020-12-21 21:53:59 -0300

    usando um mapper para parametros

--- hr-payroll/src/main/java/com/roberto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/services/PaymentService.java
@@ -1,11 +1,14 @@
 package com.roberto.hrpayroll.services;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.roberto.hrpayroll.entities.Payment;
+import com.roberto.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
@@ -16,8 +19,12 @@ public class PaymentService {
 	
 	@Autowired
 	private RestTemplate restTemplate;
-	
+		
 	public Payment getPayment(long workerId, int days) {
+		Map<String,String> uriVariable;
+		
+		
+		Worker worker = restTemplate.getForObject(,)
 		return new Payment("Bob",200.0,days);
 	}
 
