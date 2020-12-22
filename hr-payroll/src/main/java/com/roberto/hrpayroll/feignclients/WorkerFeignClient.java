56d5752d87f5cbac6cad9292b45b8827228ea51b
jrpbjr <jrpbjr@gmail.com>
2020-12-21 23:25:31 -0300

    configurando  feignClient com parametro hard code ate o momento

--- hr-payroll/src/main/java/com/roberto/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/feignclients/WorkerFeignClient.java
@@ -1,8 +1,10 @@
 package com.roberto.hrpayroll.feignclients;
 
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
 @Component
+@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
 public class WorkerFeignClient {
 
 }
