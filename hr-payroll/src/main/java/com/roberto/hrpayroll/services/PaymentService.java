2698408b65ee04ac6c084d1d8aa480b4283ff194
jrpbjr <jrpbjr@gmail.com>
2020-12-21 23:55:55 -0300

    feign client concluido

--- hr-payroll/src/main/java/com/roberto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/roberto/hrpayroll/services/PaymentService.java
@@ -1,12 +1,7 @@
 package com.roberto.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.roberto.hrpayroll.entities.Payment;
 import com.roberto.hrpayroll.entities.Worker;
@@ -21,7 +16,7 @@ public class PaymentService {
 		
 	public Payment getPayment(long workerId, int days) {
 				
-		Worker worker = 
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(),worker.getDailyIncome(), days);
 	}
 
