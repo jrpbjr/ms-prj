932776abaf3ecae01f6f1955058770400dbd5d6b
jrpbjr <jrpbjr@gmail.com>
2020-12-26 18:27:11 -0300

    Mapeamento jpa

--- hr-user/src/main/java/com/roberto/hruser/entities/User.java
+++ hr-user/src/main/java/com/roberto/hruser/entities/User.java
@@ -1,20 +1,41 @@
 package com.roberto.hruser.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
 	
-	Set<Role> role;
+	//quero que os dados sejam carregados automaticamente junto com os usuários
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id")
+			)
+	private Set<Role> role = new HashSet<>();
 	
 	public User() {
 		
@@ -59,7 +80,14 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	
 
+	public Set<Role> getRole() {
+		return role;
+	}
+
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
