7ec9512bc11fbb9e0e4f70ca44a2babbb94be89c
jrpbjr <jrpbjr@gmail.com>
2020-12-26 22:51:15 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/roberto/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/roberto/hruser/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.roberto.hruser.entities.User;
@@ -38,6 +39,14 @@ public class UserResource {
 		//logger.info("PORT = " + env.getProperty("local.server.port"));
 		
 		User obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+@GetMapping(value = "/search")	
+	
+	public ResponseEntity<User> findByEmail(@RequestParam String email){
+				
+		User obj = repository.findByEmail(email);
 		return ResponseEntity.ok(obj);
 	}
 
