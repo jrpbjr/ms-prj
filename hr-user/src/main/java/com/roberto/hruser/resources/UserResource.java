178e8b3260e681d0df136ae4ce82631dde52c8be
jrpbjr <jrpbjr@gmail.com>
2020-12-26 22:40:21 -0300

    removendo importação desnecessaria

--- hr-user/src/main/java/com/roberto/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/roberto/hruser/resources/UserResource.java
@@ -1,12 +1,10 @@
 package com.roberto.hruser.resources;
 
-import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
