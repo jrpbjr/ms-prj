0ee0413cf650bcb89e6782c2a7c90043d94be280
jrpbjr <jrpbjr@gmail.com>
2020-12-28 15:00:24 -0300

    chave secreta que vai assinar os tokens, por enquanto esta em hardcode

--- hr-oauth/src/main/java/com/roberto/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/roberto/hroauth/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.roberto.hroauth.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 public class AppConfig {
@@ -10,6 +11,13 @@ public class AppConfig {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		return tokenConverter;
 	}
 	
 }
