86a70b0c98a0432ce080a0d112be25cf93686838
jrpbjr <jrpbjr@gmail.com>
2020-12-28 13:07:35 -0300

    log para facilitar a visialização no console se encontrar o email

--- hr-oauth/src/main/java/com/roberto/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/roberto/hroauth/services/UserService.java
@@ -1,5 +1,6 @@
 package com.roberto.hroauth.services;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.roberto.hroauth.entities.User;
@@ -7,14 +8,18 @@ import com.roberto.hroauth.feignclients.UserFeignClient;
 
 public class UserService {
 	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserFeignClient userFeignClient;
 	
 	public User findByEmail(String email) {
 		User user = userFeignClient.findByEmail(email).getBody();
 		if(user == null) {
+			logger.error("Email not found" + email);
 			throw new IllegalArgumentException("Email not fount");
 		}
+		logger.info("Email found" + email);
 		return user;
 	}
 
