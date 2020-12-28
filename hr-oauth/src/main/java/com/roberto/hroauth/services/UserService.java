6b19a1b940c0507b5b93807550189131dde542ea
jrpbjr <jrpbjr@gmail.com>
2020-12-28 14:12:21 -0300

    ajustando msg log

--- hr-oauth/src/main/java/com/roberto/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/roberto/hroauth/services/UserService.java
@@ -2,10 +2,12 @@ package com.roberto.hroauth.services;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.roberto.hroauth.entities.User;
 import com.roberto.hroauth.feignclients.UserFeignClient;
 
+@Service
 public class UserService {
 	
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
@@ -17,9 +19,9 @@ public class UserService {
 		User user = userFeignClient.findByEmail(email).getBody();
 		if(user == null) {
 			logger.error("Email not found" + email);
-			throw new IllegalArgumentException("Email not fount");
+			throw new IllegalArgumentException("Email not found");
 		}
-		logger.info("Email found" + email);
+		logger.info("Email found " + email);
 		return user;
 	}
 
